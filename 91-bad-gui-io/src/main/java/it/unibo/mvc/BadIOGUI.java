5b21ff889506d619e446e37a96753a08a59d4530
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2024-11-18 11:13:19 +0100

    resolved all the stile errors

--- 91-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
+++ 91-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
@@ -6,8 +6,6 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -15,12 +13,10 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.List;
 import java.util.Random;
 
 /**
  * This class is a simple application that writes a random number on a file.
- * 
  * This application does not exploit the model-view-controller pattern, and as
  * such is just to be used to learn the basics, not as a template for your
  * applications.
@@ -31,7 +27,6 @@ public class BadIOGUI {
     private static final String PATH = System.getProperty("user.home")
             + File.separator
             + BadIOGUI.class.getSimpleName() + ".txt";
-    private static final int PROPORTION = 5;
     private final Random randomGenerator = new Random();
     private final JFrame frame = new JFrame(TITLE);
 
@@ -45,6 +40,12 @@ public class BadIOGUI {
         canvas.add(write, BorderLayout.CENTER);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JPanel horizontalPanel = new JPanel();
+        horizontalPanel.setLayout(new BoxLayout(horizontalPanel, BoxLayout.X_AXIS));
+        horizontalPanel.add(write);
+        final JButton read = new JButton("Read on file");
+        horizontalPanel.add(read);
+        canvas.add(horizontalPanel, BorderLayout.CENTER);
         /*
          * Handlers
          */
@@ -60,9 +61,21 @@ public class BadIOGUI {
                  */
                 try (PrintStream ps = new PrintStream(PATH, StandardCharsets.UTF_8)) {
                     ps.print(randomGenerator.nextInt());
-                } catch (IOException e1) {
-                    JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
-                    e1.printStackTrace(); // NOPMD: allowed as this is just an exercise
+                } catch (IOException ioException) {
+                    JOptionPane.showMessageDialog(frame, ioException, "Error", JOptionPane.ERROR_MESSAGE);
+                    ioException.printStackTrace(); // NOPMD: allowed as this is just an exercise
+                }
+            }
+        });
+        read.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    final var list = Files.readAllLines(new File(PATH).toPath());
+                    System.out.println(list); // NOPMD: allowed as this is just an exercise
+                } catch (IOException ioException) {
+                    JOptionPane.showMessageDialog(frame, ioException, "Error", JOptionPane.ERROR_MESSAGE);
+                    ioException.printStackTrace(); // NOPMD: allowed as this is just an exercise
                 }
             }
         });
@@ -77,10 +90,11 @@ public class BadIOGUI {
          * issue). It is MUCH better than manually specify the size of a window
          * in pixel: it takes into account the current resolution.
          */
-        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-        final int sw = (int) screen.getWidth();
-        final int sh = (int) screen.getHeight();
-        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+//        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+//        final int sw = (int) screen.getWidth();
+//        final int sh = (int) screen.getHeight();
+//        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.pack();
         /*
          * Instead of appearing at (0,0), upper left corner of the screen, this
          * flag makes the OS window manager take care of the default positioning
