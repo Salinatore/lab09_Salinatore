21ce8e9edcba75abbe21bc3f0f69ebe159696b5e
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2024-11-19 11:22:43 +0100

    implemented all controller api method using a String for the next string to be printed and a List<String> for history

---
+++ 93-mvc-io/src/main/java/it/unibo/mvc/controller/SimpleController.java
@@ -0,0 +1,42 @@
+package it.unibo.mvc.controller;
+
+import it.unibo.mvc.api.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 
+ *
+ */
+public final class SimpleController implements Controller {
+    private String currentString;
+    private final List<String> allAllPrintedStringHistory = new ArrayList<>();
+
+    @Override
+    public void setNextStringToPrint(final String nextStringToPrint) {
+        this.currentString = nextStringToPrint;
+    }
+
+    @Override
+    public String getNextStringToPrint() {
+        return this.currentString;
+    }
+
+    @Override
+    public List<String> getAllPrintedStringHistory() {
+        return List.copyOf(this.allAllPrintedStringHistory);
+    }
+
+    @Override
+    public void printStdoutCurrentString() throws IllegalStateException {
+        if (Objects.isNull(this.currentString)) {
+            throw new IllegalStateException("String must be not null");
+        } else {
+            System.out.println(this.currentString);
+            allAllPrintedStringHistory.add(this.currentString);
+            this.currentString = null;
+        }
+    }
+}
